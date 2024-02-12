e6cd55c31815aca929c28f4dc2033c1e4a265d2a
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-12 15:50:04 +0000

    Truthy Count

--- index1.js
+++ index1.js
@@ -285,3 +285,15 @@ function showNumbers(limit) {
 }
 
 showNumbers(20);
+
+//count truthy
+
+const array = [0, null, undefined, "", 2, 3];
+console.log(countTruthy(array));
+function countTruthy(array) {
+  let count = 0;
+  for (let value of array) {
+    if (value) count++;
+  }
+  return count;
+}

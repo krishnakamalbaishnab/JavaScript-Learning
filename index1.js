b6c31d4ec5fc3686fb81f254670fcf233a6aca00
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-12 12:55:24 +0000

    FizzBuzz Algorithm

--- index1.js
+++ index1.js
@@ -224,3 +224,22 @@ console.log(type);
 function landScape(width, height) {
   return width > height ? "LandScape" : "Not LandScape";
 }
+
+//fizzbuzz
+
+//Divisible by 3 => Fizz
+//Divisible by 5 => Buzz
+//Divisible by 3 and 5 => FizzBuzz
+//Not divisible by 3 or 5 => input
+//Not a number => 'Not a number'
+
+const output = fizzBuzz(9);
+console.log(output);
+
+function fizzBuzz(input) {
+  if (typeof input !== "number") return "Not a number";
+  if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+  if (input % 3 === 0) return "Fizz";
+  if (input % 5 === 0) return "Buzz";
+  return input;
+}

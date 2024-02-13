1127a8462dfc54711d597bd4bf11c7f2ee7d922b
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-13 10:42:38 +0000

    PartOne Conditionals Completed

--- index1.js
+++ index1.js
@@ -320,3 +320,67 @@ showProperties(movie);
 //the above code will only print the properties which are of type string and not the properties which are of type number
 
 //sum of multiples of 3 and 5
+
+function sum(limit) {
+  let sum = 0;
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
+  }
+  return sum;
+}
+
+console.log(sum(10));
+
+//grade calculation
+
+const marks = [80, 80, 50];
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks) {
+  const average = calculateAverage(marks);
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
+}
+
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of array) {
+    sum += value;
+  }
+  return sum / array.length;
+}
+
+//show stars
+
+showStars(5);
+
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+    let pattern = "";
+    for (let i = 0; i < row; i++) {
+      pattern += "*";
+    }
+    console.log(pattern);
+  }
+}
+
+//show prime numbers
+
+showPrimes(20);
+
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++) {
+    if (isPrime(number)) console.log(number);
+  }
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++) {
+    if (number % factor === 0) return false;
+  }
+  return true;
+}

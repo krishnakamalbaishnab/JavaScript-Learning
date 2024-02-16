1c2d853a1de0128a26766a872a4a28ddfe343fae
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-16 19:14:39 +0000

    Constructor functions

--- index2.js
+++ index2.js
@@ -53,3 +53,64 @@ console.log(circle1);
 
 const circle2 = createCircle(2);
 console.log(circle2);
+
+//Constructor Function
+
+//what is a constructor function?
+
+//A constructor function is a function that returns an object or a new instance of an object
+
+//Constructor functions are used to create multiple objects with the same structure and behavior.
+
+//In JavaScript, we can use the new operator to create a new instance of an object.
+
+//The new operator works as follows:
+
+//It creates a new empty object.
+
+//It sets the value of the this keyword to the new object.
+
+//It calls the constructor function to assign properties to the new object.
+
+//It returns the new object.
+
+//The new operator is used to create a new instance of an object.
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const circle3 = new Circle(1);
+console.log(circle3);
+const circle4 = new Circle(2);
+console.log(circle4);
+
+// Notes:  Constructor functions and factory functions are both used to create objects in JavaScript, but they have some key differences:
+
+// Syntax:
+
+// Constructor functions use the new keyword to create instances of objects.
+// Factory functions are regular functions that return an object.
+// Instance Creation:
+
+// Constructor functions create new instances using the new keyword followed by the function name.
+// Factory functions are called like any regular function to create new instances.
+// Return Value:
+
+// Constructor functions implicitly return this (the newly created object) if a return statement is not explicitly used.
+// Factory functions explicitly return an object.
+// Purpose:
+
+// Constructor functions are primarily used to create instances of a specific type of object (e.g., instances of a Circle constructor function represent circles).
+// Factory functions are used to create various types of objects based on the input parameters or some other conditions.
+// Inheritance:
+
+// Constructor functions can be used in conjunction with prototypes to implement inheritance in JavaScript.
+// Factory functions can also be used with other patterns like object composition to achieve similar results to inheritance.
+// this Context:
+
+// In constructor functions, this refers to the newly created instance of the object.
+// Factory functions do not have a this context unless explicitly specified.

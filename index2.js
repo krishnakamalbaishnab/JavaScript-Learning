f4722d3a2359a33609434e7f6837104b8a1dd217
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-16 18:54:12 +0000

    Notes updated

--- index2.js
+++ index2.js
@@ -33,6 +33,18 @@ function createCircle(radius) {
   };
 }
 
+//this is basically a object inside a function that creates an object and returns it
+
+//The createCircle function is designed to create circle objects. It takes a radius parameter and returns an object with a radius property and a draw method.
+
+//It does so by returning an object literal that contains two properties: radius and draw.
+
+//The radius property is assigned the value of the radius parameter passed to the function.
+
+//The draw property is a function that logs "draw" to the console.//this is a method
+
+//When you create an object inside a function and return it from that function, it's often referred to as a factory function.
+
 //in the above function what we did is that we created a function called createCircle and inside that function we created an object and returned that object
 
 const circle1 = createCircle(1);
